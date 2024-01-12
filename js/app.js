85df55c3810cc408d8ff041f67e50f4bcf2edf00
Chimennadi <nnadichime04@gmail.com>
2024-01-12 07:13:39 +0100

    code refactoring

--- js/app.js
+++ js/app.js
@@ -85,18 +85,13 @@ class UI {
 
     <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
     <h5 class="expense-amount mb-0 list-item">$${expense.amount}</h5>
-
     <div class="expense-icons list-item">
-
-     <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
-      <i class="fas fa-edit"></i>
-     </a>
-     <a href="#" class="delete-icon" data-id="${expense.id}">
-      <i class="fas fa-trash"></i>
-     </a>
+      <a href="#" class="edit-icon mx-2" data-id="${expense.id}"><i class="fas fa-edit"></i></a>
+      <a href="#" class="delete-icon" data-id="${expense.id}"><i class="fas fa-trash"></i></a>
     </div>
-   </div`;
-   this.expenseList.appendChild(div);
+    </div`;
+
+    this.expenseList.appendChild(div);
   }
 
   //total expense
